6153df82c9dbd79fac3df93fb5d4bd8e7ba1acdc
piroshiki136 <yuumaninoki@gmail.com>
2025-05-10 01:02:12 +0900

    いったん変更

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # voicevoxをバックグラウンドで起動
-/usr/local/bin/voicevox_engine &
+python3 run.py --use_gpu --voicelib_dir /opt/voicevox_core/ --runtime_dir /opt/onnxruntime/lib --host 0.0.0.0 &
+
+# 少し待機してからmain.pyを実行
+sleep 5
 
 # Pythonプログラムを起動
 python3 /scr/app/main.py
